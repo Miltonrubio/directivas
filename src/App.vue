85bef4354dd776619eeda9627374d8e00f0f3f1a
MiltonRubio <a3519110011@alumno.uttehuacan.edu.mx>
2022-09-26 14:35:55 -0500

    Primer actualizacion

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,21 @@
 <hr>
 <h2>V-SHOW</h2>
 <h3 >Tu edad es: {{edad}} </h3>
-<h3 v-show="edad>=18">Eres mayor de edad</h3>
-<h3 v-show="edad<18">Eres menor de edad</h3>
-
-<hr>
+<h3 v-if="edad>=18 && edad >0 && edad <80 ">Eres mayor de edad</h3>
+<h3 v-if="edad<18 && edad >0 && edad <80">Eres menor de edad</h3>
+<h3 v-else >Edad invalida</h3>
 
 <button @click="agreg()"> Agregar edad</button>
+
+<button @click="quit()"> Quitar edad</button>
+
+<hr>
+<h2 >v-for</h2>
+
+<ul v-for="usuario in usuarios">
+  <li>{{usuario}}</li>
+</ul>
+
 
 </template>
 
@@ -41,7 +50,9 @@
     data:()=> ({
       titulo:"Ola desde vue.js 3",
       html:"<input type='text'> Ola!",
-      edad: 14,
+      edad: 5,
+      usuarios: ["Melissa", "Fernando","Raul", "José"],
+
     }),
     methods:{
       agreg(){
